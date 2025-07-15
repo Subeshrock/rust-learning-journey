c0b2e5a1095cc32b0169f0eb5aff0c90b2a931ec
Subesh Yadav <subesh.rock.3@gmail.com>
2025-07-15 21:20:12 +0530

    add Cargo.toml, Cargo.lock, and main.rs with initial examples and struct implementations

---
+++ Day006/src/main.rs
@@ -0,0 +1,129 @@
+// Normal Example
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// Example with tuple
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+// Example with struct
+
+// #[derive(Debug)] // Derive the Debug trait to print the struct
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!("rect1 is {rect1:?}");
+//     println!("rect1 is {rect1:#}");
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&rect1)
+//     );
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     dbg!(&rect1);
+// }
+
+
+
+// Methods Example
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
+
+    fn max(self, other: Rectangle) -> Rectangle {
+        Rectangle { 
+            width: self.width.max(other.width),
+            height: self.height.max(other.height),
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+}
