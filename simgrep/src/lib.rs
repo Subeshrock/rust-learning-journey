5235ccea09e32d1eb76f708c65847a63cae9539e
Subesh Yadav <subesh.rock.3@gmail.com>
2025-08-03 16:10:57 +0530

    Add initial implementation of Cargo.lock, Cargo.toml, and main.rs for Day023 with iterator trait and example usage

--- simgrep/src/lib.rs
+++ simgrep/src/lib.rs
@@ -24,13 +24,24 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments. Usage: <query> <filename>");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        // if args.len() < 3 {
+        //     return Err("Not enough arguments. Usage: <query> <filename>");
+        // }
+        // let query = args[1].clone();
+        // let filename = args[2].clone();
 
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
@@ -42,10 +53,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
+    // contents
+    //     .lines()
+    //     .filter(|line| line.contains(query))
+    //     .collect()
     // let mut results = Vec::new();
     // for line in contents.lines() {
     //     if line.contains(query) {
@@ -54,6 +65,8 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     // }
 
     // results
+
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- simgrep/src/main.rs
+++ simgrep/src/main.rs
@@ -4,11 +4,11 @@ use std::process;
 use simgrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
     // let query = args.get(1).unwrap_or(&"default_query".to_string());
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
